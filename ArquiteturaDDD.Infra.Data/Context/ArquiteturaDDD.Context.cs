fa49a1427968665516002a54834091c929bc91ea
caique  coelho pinheiro <caiquepinheiro69@gmai.com>
2024-03-25 18:50:22 -0300

    Add project files.

---
+++ ArquiteturaDDD.Infra.Data/Context/ArquiteturaDDD.Context.cs
@@ -0,0 +1,22 @@
+﻿using ArquiteturaDDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+
+namespace ArquiteturaDDD.Infra.Data.Context
+{
+    public class ArquiteturaDDD : DbContext
+    { 
+        public ArquiteturaDDD()
+        : base ("ArquiteturaDDDOne")
+        {
+        
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }    
+    }
+}
